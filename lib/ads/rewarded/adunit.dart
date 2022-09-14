41ddb5bc81bd8a1b8143edc3f4d7fe82743469f6
Martin <47219299+hepnn@users.noreply.github.com>
2022-09-14 22:36:42 +0300

    Update adunit.dart

--- lib/ads/rewarded/adunit.dart
+++ lib/ads/rewarded/adunit.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdHelper {
   static String get rewardedAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/8673189370';
+      return ''; // enter your own id
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/7552160883';
+      return '';// enter your own id
     } else {
       throw UnsupportedError('Unsupported platform');
     }
